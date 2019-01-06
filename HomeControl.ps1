80aa3eabed02f18765279552e791872f35edb9a6
Matt Gelder <matt_gelder@hotmail.com>
2019-01-06 15:44:09 +0000

    Find MAC addresses

--- HomeControl.ps1
+++ HomeControl.ps1
@@ -1,2 +1,10 @@
+#git init
+#git remote add PiHomeControl https://github.com/mrmatt001/PiHomeControl.git
+#git push --set-upstream PiHomeControl master
+
+
 modprobe btusb
 systemctl start bluetooth
+$MACAddresses = @()
+foreach ($Device in ("devices" | bluetoothctl)) { if ($Device -match 'CC-RT-M-BLE') { if ($Device -match '(?<MACAddress>[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+)') { $MACAddresses += $Matches.MACAddress } } }
+$MACAddresses | Select-Object -Unique
