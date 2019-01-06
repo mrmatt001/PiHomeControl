8fd1b5baf3252eaba74542cb93de6b0fd58ad05b
Matt Gelder <matt_gelder@hotmail.com>
2019-01-06 21:31:32 +0000

    Get-Temp Logic

--- HomeControl.ps1
+++ HomeControl.ps1
@@ -9,37 +9,63 @@ $MACAddresses = @()
 foreach ($Device in ("devices" | bluetoothctl)) { if ($Device -match 'CC-RT-M-BLE') { if ($Device -match '(?<MACAddress>[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+)') { $MACAddresses += $Matches.MACAddress } } }
 foreach ($MACAddress in ($MACAddresses | Select-Object -Unique))
 {
-    foreach ($Line in ("info $MACAddress" | bluetoothctl))
+    try
     {
-        $PairedStatus = $false
-        if ($Line -match 'Paired\:\s+(?<PairedStatus>[a-zA-Z]+)') 
+        get-job | Remove-Job -Force
+        $ScriptBlock = (gatttool -b $MACAddress --char-write-req  -a "0x0411" -n "03")
+        $JobList = @()
+        $Job = (Start-Job -ScriptBlock $SB -Name $MACAddress)
+        do
         {
-            if ($Matches.PairedStatus -eq 'no') 
+            start-sleep -milliseconds 500
+            $JobOutput = Get-Job -id $Job.Id | Receive-Job -Keep
+        }
+        until ($JobOutput -match 'Characteristic')
+
+        $TempOutput = Get-Job -id $Job.ID | Receive-Job
+        Get-Job -id $Job.Id | Remove-Job -Force
+        foreach ($Line in $TempOutput)
+        {
+            if ($Line -match 'Notification handle\s+\=\s+\dx\d+\svalue:\s[a-zA-Z0-9]+\s[a-zA-Z0-9]+\s[a-zA-Z0-9]+\s[a-zA-Z0-9]+\s[a-zA-Z0-9]+\s(?<Temp>[a-zA-Z0-9]+)') 
+            { 
+                Write-Host ("Temperature: " + [Convert]::ToInt64(($Matches.Temp),16)/2 + "C")
+            }
+        }
+    }
+    catch
+    {
+        foreach ($Line in ("info $MACAddress" | bluetoothctl))
+        {
+            $PairedStatus = $false
+            if ($Line -match 'Paired\:\s+(?<PairedStatus>[a-zA-Z]+)') 
             {
-                Write-Host ("$MACAddress is not paired") -ForegroundColor Red
-                Write-Host ("$MACAddress attempting to pair")
-                "pair $MACAddress" | bluetoothctl
-                Start-Sleep -Seconds 5
-                foreach ($Line in ("info $MACAddress" | bluetoothctl))
+                if ($Matches.PairedStatus -eq 'no') 
                 {
-                    if ($Line -match 'Paired\:\s+(?<PairedStatus>[a-zA-Z]+)') 
+                    Write-Host ("$MACAddress is not paired") -ForegroundColor Red
+                    Write-Host ("$MACAddress attempting to pair")
+                    "pair $MACAddress" | bluetoothctl
+                    Start-Sleep -Seconds 5
+                    foreach ($Line in ("info $MACAddress" | bluetoothctl))
                     {
-                        if ($Matches.PairedStatus -eq 'no') 
+                        if ($Line -match 'Paired\:\s+(?<PairedStatus>[a-zA-Z]+)') 
                         {
-                            Write-Host ("$MACAddress could not be paired") -ForegroundColor Red
-                        }
-                        else 
-                        {
-                            Write-Host ("$MACAddress is paired") -ForegroundColor Green  
-                            $PairedStatus = $true
+                            if ($Matches.PairedStatus -eq 'no') 
+                            {
+                                Write-Host ("$MACAddress could not be paired") -ForegroundColor Red
+                            }
+                            else 
+                            {
+                                Write-Host ("$MACAddress is paired") -ForegroundColor Green  
+                                $PairedStatus = $true
+                            }
                         }
                     }
                 }
-            }
-            else 
-            {
-                Write-Host ("$MACAddress is paired") -ForegroundColor Green
-                $PairedStatus = $true
+                else 
+                {
+                    Write-Host ("$MACAddress is paired") -ForegroundColor Green
+                    $PairedStatus = $true
+                }
             }
         }
     }
