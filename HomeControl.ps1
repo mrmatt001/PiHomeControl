2a7653f14db6b6144e9417bd8c278e070876655a
Matt Gelder <matt_gelder@hotmail.com>
2019-01-06 16:04:54 +0000

    add info

--- HomeControl.ps1
+++ HomeControl.ps1
@@ -8,3 +8,7 @@ systemctl start bluetooth
 $MACAddresses = @()
 foreach ($Device in ("devices" | bluetoothctl)) { if ($Device -match 'CC-RT-M-BLE') { if ($Device -match '(?<MACAddress>[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+\:[0-9A-Z]+)') { $MACAddresses += $Matches.MACAddress } } }
 $MACAddresses | Select-Object -Unique
+foreach ($MACAddress in $MACAddresses)
+{
+    "info $MACAddress" | bluetoothctl
+}
