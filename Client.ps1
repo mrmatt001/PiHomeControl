6e638c7d2b78b04706285e8449309832ef9b4861
Matt Gelder <matt_gelder@hotmail.com>
2019-01-13 16:25:44 +0000

    Client Server

--- Client.ps1
+++ Client.ps1
@@ -1,7 +1,7 @@
 #git init
 #git remote add PiHomeControl https://github.com/mrmatt001/PiHomeControl.git
 #git push --set-upstream PiHomeControl master
-Import-Module .\EQ3PiPowerShell.psm1
+Import-Module /home/pi/PiHomeControl/EQ3PiPowerShell.psm1
 
 modprobe btusb
 systemctl start bluetooth
