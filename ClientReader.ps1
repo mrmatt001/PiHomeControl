7dd91a73c39dccb633a963200554d47976325f77
Matt Gelder <matt_gelder@hotmail.com>
2019-02-04 22:22:31 +0000

    get-eq3temp

--- ClientReader.ps1
+++ ClientReader.ps1
@@ -1,18 +1,21 @@
 Import-Module /home/pi/PiHomeControl/PiHomeControl.psm1 -Force
 
 modprobe btusb
+systemctl stop bluetooth
 systemctl start bluetooth
 hciconfig hci0 down
 hciconfig hci0 up
-$ScanOnJob = Start-Job -ScriptBlock {"scan on" | bluetoothctl }
-do { Start-Sleep -Milliseconds 100 } until ((Get-Job -Id $ScanOnJob.Id).State -eq 'Completed')
-Receive-Job -Id $ScanOnJob.Id
-Remove-Job -Id $ScanOnJob.Id
-
-$ScanOffJob = Start-Job -ScriptBlock {"scan off" | bluetoothctl }
-do { Start-Sleep -Milliseconds 100 } until ((Get-Job -Id $ScanOffJob.Id).State -eq 'Completed')
-Receive-Job -Id $ScanOffJob.Id
-Remove-Job -Id $ScanOffJob.Id
+#$ScanOnJob = Start-Job -ScriptBlock {
+    "scan on" | bluetoothctl #}
+#do { Start-Sleep -Milliseconds 100 } until ((Get-Job -Id $ScanOnJob.Id).State -eq 'Completed')
+#Receive-Job -Id $ScanOnJob.Id
+#Remove-Job -Id $ScanOnJob.Id
+Start-Sleep -Seconds 10
+#$ScanOffJob = Start-Job -ScriptBlock {
+    "scan off" | bluetoothctl #}
+#do { Start-Sleep -Milliseconds 100 } until ((Get-Job -Id $ScanOffJob.Id).State -eq 'Completed')
+#Receive-Job -Id $ScanOffJob.Id
+#Remove-Job -Id $ScanOffJob.Id
 
 foreach ($MACAddress in (Get-EQ3Thermostats)) 
 { 
--- PiHomeControl.psm1
+++ PiHomeControl.psm1
@@ -5,9 +5,9 @@ function Get-EQ3Temperature
     )
     process
     {
-        modprobe btusb
-        systemctl start bluetooth
-        #get-job | Remove-Job -Force
+        #modprobe btusb
+        #systemctl start bluetooth
+        get-job | Remove-Job -Force
         $scriptBlock = [scriptblock]::Create("gatttool -b " + $MACAddress + ' --char-write-req -a "0x0411" -n "03" --listen')
         $Job = (Start-Job -Name $MACAddress -ScriptBlock $scriptBlock -ArgumentList $MACAddress)
         $JobStartTime = (Get-Date)
