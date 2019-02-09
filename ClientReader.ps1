bc00b7287608a751b2c5b3fe860e710c52be1577
Matt Gelder <matt_gelder@hotmail.com>
2019-02-09 17:34:12 +0000

    cr

--- ClientReader.ps1
+++ ClientReader.ps1
@@ -30,8 +30,6 @@ do
             {
                 $MACAddress = $Line.Split(' ')[0].Trim()
                 $Description = $Line.Split(' ')[1].Trim()
-                $MACAddress
-                $Description
                 if ($Description -match '[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]:[0-9a-fA-F][0-9a-fA-F]')
                 {
                     if ($Description -match 'CC-RT-M-BLE') 
