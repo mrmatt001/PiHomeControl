94e2b590a92bc411654b2303bc982c191ed3c044
Matt Gelder <matt_gelder@hotmail.com>
2019-02-10 14:57:49 +0000

    cr

--- ClientReader.ps1
+++ ClientReader.ps1
@@ -71,7 +71,7 @@ do
                     
                         $ScanDate = (Get-Date)
                         $insert = "INSERT INTO pitoeq3(pihostname, eq3macaddress, lastdetected) VALUES ('$ComputerName','$_','$ScanDate')"
-                        $upsert = "UPDATE pidevices SET lastdetected='$ScanDate' WHERE pihostname='$ComputerName' AND eq3macaddress='$_'";
+                        $upsert = "UPDATE pitoeq3 SET lastdetected='$ScanDate' WHERE pihostname='$ComputerName' AND eq3macaddress='$_'";
                         $Statement = "WITH upsert AS ($upsert RETURNING *) $insert WHERE NOT EXISTS (SELECT * FROM upsert)"
                         Write-ToPostgreSQL -Statement $Statement -DBServer $DBServer -DBName $DBName -DBPort 5432 -DBUser $DBUser -DBPassword $DBPassword
                     }
